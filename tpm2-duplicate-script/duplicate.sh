1e48d4f7ff0581dd1f092e717648967ea12ac26c
Hui Li <leehui_hrbeu71102@163.com>
2019-01-21 15:14:15 +0800

    add tpm2-duplicate-script:
    - this is an independent duplicate script that uses the newest tpm2_tools
      originally duplicated from tpm2_tools/test, and expected to be merged
      to tpm2_tools/test after all function is done.

---
+++ tpm2-duplicate-script/duplicate.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#;**********************************************************************;
+#
+# Copyright (c) 2017, Alibaba Group
+# Copyright (c) 2018, Intel Corporation
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of Intel Corporation nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+#;**********************************************************************;
+export TPM2_ABRMD="tpm2-abrmd" TPM2_SIM="tpm_server" PATH="/home/tss/bin:/home/tss/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" TPM2_SIM_NV_CHIP="" TPM2_TOOLS_TEST_FIXTURES=""
+
+source helpers.sh
+
+cleanup() {
+  rm -f
+
+  ina "$@" "keep-context"
+  if [ $? -ne 0 ]; then
+    rm -f context.out
+  fi
+
+  rm -f key.ctx out.yaml
+
+  ina "$@" "no-shut-down"
+  if [ $? -ne 0 ]; then
+    shut_down
+  fi
+}
+trap cleanup EXIT
+
+start_up
+
+cleanup "no-shut-down"
+
+echo "start."
+TPM_CC_Duplicate=0x14B
+
+echo "creating ek"
+tpm2_createprimary -Q -a e -g sha256 -G rsa -o ek.ctx
+
+echo "creating aik"
+tpm2_create -Q -g sha256 -G rsa -u ak.pub -r ak.priv  -C ek.ctx
+
+echo "creating src storage key"
+tpm2_createprimary -Q -a o -g sha256 -G rsa -o src.ctx
+
+echo "creating dst storage key"
+tpm2_createprimary -Q -a o -g sha256 -G rsa -o dst.ctx
+
+echo "creating policy for keyedhash key which contains kwk"
+
+echo "  start authsession for trial policy session, Do NOT use '-a' which is used in real authorization"
+tpm2_startauthsession -Q -g sha256 -S p1.session
+
+echo "  bind trial policy to TPM_CC_Duplicate, now tpm2_policyget digest is not available, acctualy we just need to get policy digest"
+tpm2_policycommandcode -Q -S p1.session -o p1.data $TPM_CC_Duplicate
+
+echo "  load ak"
+tpm2_load -Q -C ek.ctx -u ak.pub -r ak.priv -n ak.name -o ak.ctx
+
+echo "  sign the trial policy"
+tpm2_sign -Q  -c ak.ctx -G sha256 -m p1.data -s p1.sig
+
+echo "  verify the trial policy signature"
+tpm2_verifysignature -Q -c ak.ctx -G sha256 -m p1.data -s p1.sig -t p1.tk
+
+echo "  bind policy to ak"
+tpm2_policyauthorize -Q -S p1.session -f p1.data -o p1.data.auth -n ak.name -t p1.tk
+
+echo "  flush ak and policy session"
+tpm2_flushcontext -Q -c ak.ctx -S p1.session
+
+echo "12345678" > kwk.data
+
+echo "  create keyedhash key containing fake kwk"
+tpm2_create -Q -C src.ctx -L p1.data.auth -g sha256 -u dupkey.pub -r dupkey.priv -A userwithauth -I kwk.data
+
+echo "  start authsession"
+tpm2_startauthsession -Q -g sha256 -S p2.session
+
+echo "  bind trial policy to TPM_CC_Duplicate"
+tpm2_policycommandcode -Q -S p2.session -o p2.data $TPM_CC_Duplicate
+
+echo "  read dst key name"
+tpm2_readpublic -c dst.ctx  > dst.log
+
+dst_name=`yaml_get_kv dst.log \"name\"`
+echo "  $dst_name"
+
+echo "  select duplicate destination"
+#tpm2_policy
+
+echo "end."
