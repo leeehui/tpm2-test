761392f0341c5fc49b22ad16a2e352b35728fa9f
Hui Li <leehui_hrbeu71102@163.com>
2019-03-13 00:55:09 +0800

    tpm2-duplicate-script:
    - add all steps to Unseal function
    
    Note: Unseal failed, seems key creation related
          problem as it output the same error code even
          we do NOT start a real auth session

--- tpm2-duplicate-script/duplicate.sh
+++ tpm2-duplicate-script/duplicate.sh
@@ -30,33 +30,37 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 #;**********************************************************************;
-export TPM2_ABRMD="tpm2-abrmd" TPM2_SIM="tpm_server" PATH="/home/tss/bin:/home/tss/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" TPM2_SIM_NV_CHIP="" TPM2_TOOLS_TEST_FIXTURES=""
-
-source helpers.sh
-
-cleanup() {
-  rm -f
-
-  ina "$@" "keep-context"
-  if [ $? -ne 0 ]; then
-    rm -f context.out
-  fi
-
-  rm -f key.ctx out.yaml
-
-  ina "$@" "no-shut-down"
-  if [ $? -ne 0 ]; then
-    shut_down
-  fi
-}
-trap cleanup EXIT
-
-start_up
-
-cleanup "no-shut-down"
+#export TPM2_ABRMD="tpm2-abrmd" TPM2_SIM="tpm_server" PATH="/home/tss/bin:/home/tss/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" TPM2_SIM_NV_CHIP="" TPM2_TOOLS_TEST_FIXTURES=""
+#
+#source helpers.sh
+#
+#cleanup() {
+#  rm -f
+#
+#  ina "$@" "keep-context"
+#  if [ $? -ne 0 ]; then
+#    rm -f key.ctx
+##    rm -f ak.ctx   ak.pub    dupkey.ctx         dupkey.name.imported  ek.ctx          kwk.data  p1.data.auth  p1.tk       p2.sig   p3.data.auth  p4.session  p5.data       s.duplicate \
+##    ak.name  dst.ctx   dupkey.ctx.import  dupkey.priv           encryption.out  NVChip    p1.session    p2.data     p2.tk    p3.session    p4.sig      p5.data.auth  seed.out \
+##    ak.priv  dst.name  dupkey.name        dupkey.pub            import.priv     p1.data   p1.sig        p2.session  p3.data  p4.data       p4.tk       p5.session    src.ctx
+#  fi
+#
+#  rm -f key.ctx out.yaml
+#
+#  ina "$@" "no-shut-down"
+#  if [ $? -ne 0 ]; then
+#    shut_down
+#  fi
+#}
+#trap cleanup EXIT
+#
+#start_up
+#
+#cleanup "no-shut-down"
 
 echo "start."
 TPM_CC_Duplicate=0x14B
+TPM_CC_Unseal=0x15e
 
 echo "creating ek"
 tpm2_createprimary -Q -a e -g sha256 -G rsa -o ek.ctx
@@ -75,14 +79,14 @@ echo "creating policy for keyedhash key which contains kwk"
 echo "  start authsession for trial policy session p1.session, Do NOT use '-a' which is used in real authorization"
 tpm2_startauthsession -Q -g sha256 -S p1.session
 
-echo "  bind trial policy to TPM_CC_Duplicate, now tpm2_policyget digest is not available, acctualy we just need to get policy digest"
-tpm2_policycommandcode -Q -S p1.session -o p1.data $TPM_CC_Duplicate
+echo "  bind trial policy to TPM_CC_Duplicate, now tpm2_policygetdigest is not available, acctualy we just need to get policy digest"
+tpm2_policycommandcode -Q -S p1.session -o p1.data $TPM_CC_Unseal
 
 echo "  load ak"
 tpm2_load -Q -C ek.ctx -u ak.pub -r ak.priv -n ak.name -o ak.ctx
 
 echo "  sign the trial policy p1"
-tpm2_sign -Q  -c ak.ctx -G sha256 -m p1.data -s p1.sig
+tpm2_sign -Q  -c ak.ctx -G sha256 -m p1.data -o p1.sig
 
 echo "  verify the trial policy signature"
 tpm2_verifysignature -Q -c ak.ctx -G sha256 -m p1.data -s p1.sig -t p1.tk
@@ -93,10 +97,10 @@ tpm2_policyauthorize -Q -S p1.session -f p1.data -o p1.data.auth -n ak.name -t p
 echo "  flush ak and policy session"
 tpm2_flushcontext -Q -c ak.ctx -S p1.session
 
-echo "12345678" > kwk.data
+echo "1234567" > kwk.data
 
 echo "create keyedhash key containing fake kwk"
-tpm2_create -Q -C src.ctx -L p1.data.auth -g sha256 -u dupkey.pub -r dupkey.priv -A userwithauth -I kwk.data
+tpm2_create -Q -C src.ctx -L p1.data.auth -g sha256 -u dupkey.pub -r dupkey.priv -A userwithauth -i kwk.data
 
 echo "duplicate keyedhash key"
 
@@ -112,7 +116,7 @@ tpm2_readpublic -Q -c dst.ctx -n dst.name #> dst.log
 #echo "  $dst_name"
 
 echo "  select duplicate destination"
-tpm2_policyduplicationselect -Q -i -S p2.session -n dupkey.name -p dst.name
+tpm2_policyduplicationselect -Q -i -S p2.session -n dupkey.name -N dst.name
 
 #Note: policyduplicationselect do NOT allow any command code exists in policy sessioin
 #      according to command spec. this command will set policySession->commandCode to TPM_CC_Duplicate
@@ -128,7 +132,7 @@ echo "  load ak"
 tpm2_load -Q -C ek.ctx -u ak.pub -r ak.priv -n ak.name -o ak.ctx
 
 echo "  sign the trial policy p2"
-tpm2_sign -Q  -c ak.ctx -G sha256 -m p2.data -s p2.sig
+tpm2_sign -Q  -c ak.ctx -G sha256 -m p2.data -o p2.sig
 
 echo "  verify the trial policy signature"
 tpm2_verifysignature -Q -c ak.ctx -G sha256 -m p2.data -s p2.sig -t p2.tk
@@ -140,7 +144,7 @@ echo "  start real authsession p3.session"
 tpm2_startauthsession -Q -a -g sha256 -S p3.session
 
 echo "  select duplicate destination"
-tpm2_policyduplicationselect -Q -i -S p3.session -n dupkey.name -p dst.name
+tpm2_policyduplicationselect -Q -i -S p3.session -n dupkey.name -N dst.name
 
 echo "  bind trial policy to TPM_CC_Duplicate"
 tpm2_policycommandcode -Q -S p3.session -o p3.data $TPM_CC_Duplicate
@@ -150,5 +154,59 @@ tpm2_policyauthorize -Q -S p3.session -f p3.data -o p3.data.auth -n ak.name -t p
 
 echo "  do duplication"
 tpm2_duplicate -C dst.ctx -c dupkey.ctx -p session:p3.session -K encryption.out -d s.duplicate -s seed.out
+
+echo "  import the duplicated key"
+tpm2_import -g sha256 -C dst.ctx -d s.duplicate -s seed.out -u dupkey.pub -r import.priv
+
+echo "  flush authsession p3.session"
+#tpm2_flushcontext -Q -S p3.session
+
+echo "  start trial authsession p4.session"
+tpm2_startauthsession -Q -g sha256 -S p4.session
+
+#echo "  bind trial policy to Locality 3"
+#tpm2_policylocality -Q -S p4.session -o p4.data 3
+#tpm2_policypcr -Q -S p4.session -L sha256:0 -f p4.data
+tpm2_policycommandcode -Q -S p4.session -o p4.data 0x15e
+
+echo "  flush authsession p4.session"
+tpm2_flushcontext -Q -S p4.session
+
+rm ak.name ak.ctx
+echo "  load ak"
+tpm2_load -Q -C ek.ctx -u ak.pub -r ak.priv -n ak.name -o ak.ctx
+
+echo "  sign the trial policy p4"
+tpm2_sign -Q  -c ak.ctx -G sha256 -m p4.data -o p4.sig
+
+echo "  verify the trial policy signature"
+tpm2_verifysignature -Q -c ak.ctx -G sha256 -m p4.data -s p4.sig -t p4.tk
+
+echo "  flush ak"
+tpm2_flushcontext -Q -c ak.ctx
+
+echo "  load keyedhash key(imported)"
+tpm2_load -Q -C dst.ctx -u dupkey.pub -r import.priv -n dupkey.name.imported -o dupkey.ctx.import
+
+echo "  start real authsession p5.session"
+tpm2_startauthsession -Q -a -g sha256 -S p5.session
+
+#echo "  bind trial policy to Locality 3"
+#tpm2_policylocality -Q -S p5.session -o p5.data 3
+
+#tpm2_policypcr -Q -S p5.session -L sha256:0  -f p5.data
+tpm2_policycommandcode -Q -S p5.session -o p5.data 0x15e
+
+echo "  bind policy to ak"
+tpm2_policyauthorize -Q -S p5.session -f p5.data -o p5.data.auth -n ak.name -t p4.tk
+
+#echo "  set locality to 3"
+#tpm2_setlocality -Q  3
+
+echo "  tpm2_unseal kwk"
+tpm2_unseal -c dupkey.ctx.import
+
+echo "  flush authsession p5.session"
+#tpm2_flushcontext -Q -S p5.session
 
 echo "end."
